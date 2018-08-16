a9e6bdf34cb7deda69467106982ba8df5a05333d
docoding <developer70@indglobal-consulting.com>
2018-08-16 19:42:03 +0530

    1mg_handler result of 1stStage Filter

--- 1mg_handler.py
+++ 1mg_handler.py
@@ -1,7 +1,6 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import csv
-import string
 
 
 def get_soup(url_string):
@@ -9,3 +8,23 @@ def get_soup(url_string):
     page_content = web_client.read()
     web_client.close()
     return soup(page_content, 'html5lib')
+
+
+url = 'https://www.1mg.com/search/all?name='
+drug = 'Bacitracin'
+
+for element in get_soup(url+drug).find_all('div', {'class', 'SkuInfoCards__div-info-card___2vtcl'}):
+    try:
+        result_name = element.find('div', {'class': 'SkuInfoCards__info-header___1966o'}).text
+        result_detail = element.find('div', {'class': 'SkuInfoCards__sku-info___3D-Dm'}).text
+
+        print(result_name, '[', result_detail, ']')
+
+        # with open('1mg_search_result.csv', 'w', newline='') as mg_search_result_file:
+        #     writer = csv.writer(mg_search_result_file, delimiter=',')
+        #     writer.writerow([])
+    except AttributeError:
+        result_detail = ''
+        result_name = element.find('div', {'class': 'SkuInfoCards__info-header___1966o'}).text
+
+        print(result_name, '[', result_detail, ']')
